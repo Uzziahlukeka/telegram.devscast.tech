62a39ab2a57fbbfd734a501cea737f00aa5094e3
uzziahlukeka <uzziahlukeka@gmail.com>
2024-09-25 14:27:21 +0200

    commit

--- src/Handler/Devscast/ListDevscastUnreadEmailHandler.php
+++ src/Handler/Devscast/ListDevscastUnreadEmailHandler.php
@@ -83,7 +83,7 @@ MESSAGE;
         $inbox = $this->connection->getMailbox('INBOX');
         return $inbox->getMessages(
             search: new Unseen(),
-            sortCriteria: \SORTDATE, // https://php.net/manual/en/imap.constants.php
+            sortCriteria: \SORTDATE, //-> https://php.net/manual/en/imap.constants.php
             descending: true
         );
     }
